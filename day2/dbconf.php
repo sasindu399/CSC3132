72b712c6b884583bfedaa8c599ac6c55cd1d5f59
sasindu399 <139614499+sasindu399@users.noreply.github.com>
2024-12-03 01:55:47 +0000

    implement the code

---
+++ day2/dbconf.php
@@ -0,0 +1,20 @@
+<?php
+$dbHost = getenv('DB_HOST');
+$dbName = getenv('DB_NAME');
+$dbUser = getenv('DB_USER');
+$dbPass = getenv('DB_PASS');
+
+try {
+	$connect = mysqli_connect($dbHost,$dbName,$dbUser,$dbPass);
+	if (!$connect) {
+		die("connection failed".mysqli_connect_error());
+	} 
+	else {
+		//echo "Connected successfully";
+	}
+} 
+catch (Exception $e) {
+	die($e->getMessage());
+}
+
+?>
--- public/dbconf.php
+++ public/dbconf.php
@@ -1,12 +1,10 @@
 <?php
-//constant variable
 $dbHost = getenv('DB_HOST');
 $dbName = getenv('DB_NAME');
 $dbUser = getenv('DB_USER');
 $dbPass = getenv('DB_PASS');
 
 try {
-	//connect with database
 	$connect = mysqli_connect($dbHost,$dbName,$dbUser,$dbPass);
 	if (!$connect) {
 		die("connection failed".mysqli_connect_error());
@@ -18,7 +16,5 @@ try {
 catch (Exception $e) {
 	die($e->getMessage());
 }
-
-//echo "abc<br>";
 
 ?>
